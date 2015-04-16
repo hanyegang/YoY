d7b041689443ec782762ebf2621b7277fa9ccb22
hanyegang <1209408295@qq.com>
2015-04-16 11:38:39 +0800

    111

---
+++ YoY/YoY/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YoY
+//
+//  Created by hanyegang on 15/4/16.
+//  Copyright (c) 2015年 hanyegang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YoY/YoY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YoY
+//
+//  Created by hanyegang on 15/4/16.
+//  Copyright (c) 2015年 hanyegang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
